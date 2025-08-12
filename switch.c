24cd65f211db6e6a9b4fe32caf4ea7c915457c51
Rodrigo Ramos <jupuschnerat@gmail.com>
2025-08-12 19:07:06 -0300

    alguns switches

---
+++ switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int variavel;
+
+  printf("Digite um valor: \n");
+  scanf("%d", &variavel);
+
+  switch (variavel) {
+  case 1:
+    printf("Código a ser executado se variavel é == 1\n");
+  break;
+  case 2:
+    printf("Código a ser executado se variavel é == 2\n");
+  break;
+  default:
+    printf("Código a ser executado se a variavel não for 1 ou 2.\n");
+  }
+}
---
+++ switch_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int dia;
+
+  printf("Entre o valor do dia!\n");
+  scanf("%d", &dia);
+}
