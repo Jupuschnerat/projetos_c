6d7c730a88f5b0252fc3d718ca4b92918740dbc0
Rodrigo Ramos <jupuschnerat@gmail.com>
2025-08-15 14:45:34 -0300

    uso do switch

--- switch_2.c
+++ switch_2.c
@@ -5,4 +5,20 @@ int main() {
 
   printf("Entre o valor do dia!\n");
   scanf("%d", &dia);
+
+  if(dia == 1) {
+    printf("Domingo\n");
+  } else if(dia == 2) {
+    printf("Segunda-feira\n");
+  } else if(dia == 3) {
+    printf("Terça-feira\n");
+  } else if(dia == 4) {
+    printf("Quarta-feira\n");
+  } else if(dia == 5) {
+    printf("Quinta-feira\n");
+  } else if(dia == 6) {
+    printf("Sexta-feira\n");
+  } else {
+    printf("Sábado\n");
+  }
 }
